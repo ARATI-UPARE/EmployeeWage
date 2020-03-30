5bdfc91785c4fed024bcc8061650e086b9b8f14f
ARATI-UPARE <aratiupare@gmail.com>
2020-03-30 11:30:52 +0530

    ADDED:Calculate Emplyee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,19 +1,29 @@
-// WAP to Dislpay Employee is present or absent
+// WAP to Calculate Daily Wage of Employee
 
 public class EmployeeWage {
     public static void main(String arg[]){
         System.out.println("WELCOME to EMPLOYEE WAGE Computation");
+        // Constants
+        int empRatePerHr=20;
+        int empHrs=0;
+        // Variable
+        int empWage=0;
+
         double employee=Math.random();
-        System.out.println("Employee is Preswnt");
-        System.out.println(employee);
         // employee presnt if value is greater than 0.5
         if(employee>0.5)
         {
             System.out.println("Employee is Present");
+            empHrs=8;
+            empWage=(empHrs*empRatePerHr);
+            System.out.println("Employee Wage is "+empWage);
         }
         else
         {
             System.out.println("Employee is Absent");
+            empHrs=0;
+            empWage=(empHrs*empRatePerHr);
+            System.out.println("Employee Wage is "+empWage);
         }
     }
 }
