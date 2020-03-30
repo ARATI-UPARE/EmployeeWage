37cb5fa44ecfeafc1cdfe848ee85a4bae68b6fe3
ARATI-UPARE <aratiupare@gmail.com>
2020-03-30 11:11:50 +0530

    ADDED:Employee is present or absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,19 @@
+// WAP to Dislpay Employee is present or absent
+
 public class EmployeeWage {
     public static void main(String arg[]){
         System.out.println("WELCOME to EMPLOYEE WAGE Computation");
+        double employee=Math.random();
+        System.out.println("Employee is Preswnt");
+        System.out.println(employee);
+        // employee presnt if value is greater than 0.5
+        if(employee>0.5)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
     }
 }
