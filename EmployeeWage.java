9f3e6904144c0d5a9e408d52ced73148af6d4150
ARATI-UPARE <aratiupare@gmail.com>
2020-03-30 17:55:05 +0530

    ADDED:Employee Daily Wage using switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,29 +1,32 @@
 // WAP to Calculate Daily Wage of Employee
 
 public class EmployeeWage {
+    // Constants
+    public static final int empRatePerHr=20;
+    public static final int is_Part_Time=2;
+    public static final int is_Full_Time=1;
+
     public static void main(String arg[]){
+
         System.out.println("WELCOME to EMPLOYEE WAGE Computation");
-        // Constants
-        int empRatePerHr=20;
-        int is_Part_Time=2;
-        int is_Full_Time=1;
 
         // Variable
         int empHrs=0;
         int empWage=0;
-        // Employee check
-        double employee = (int)(( Math.random() * 10 ) % 3);
-        if ( employee == is_Part_Time ) {
-            System.out.println("Part Time  ");
-            empHrs = 4;
-        }
-        else if (employee == is_Full_Time ) {
-            System.out.println("Full Time ");
-            empHrs = 8;
-        }
-        else {
-            System.out.println("Employee is Absent ");
-            empHrs = 0;
+
+        int employee=(int)(( Math.random()*10)%3);
+        switch(employee){
+            case is_Part_Time:
+                System.out.println("Part Time  ");
+                empHrs = 4;
+                break;
+            case is_Full_Time:
+                System.out.println("Full Time ");
+                empHrs = 8;
+                break;
+            default:
+                System.out.println("Employee is Absent ");
+                empHrs = 0;
         }
             empWage=(empHrs*empRatePerHr);
             System.out.println("Employee Wage is "+empWage);
