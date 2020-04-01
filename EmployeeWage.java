6d5bd71d39dc23596779b1c69ea0b9855a4a9491
ARATI-UPARE <aratiupare@gmail.com>
2020-04-02 00:23:11 +0530

    ADDED:Store Daily Wage along with Total wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -13,14 +13,20 @@ public class EmployeeWage implements EmpWageBuilder{
     final int is_Part_Time = 1;
     final int is_Full_Time = 2;
 
+    // ArrayList to Store daily Wage along with Monthly Wage
+
+    ArrayList<Integer> empDailyMonthlyTotalWage = new ArrayList<Integer>();
+
     // Method to compute employee wage
     public void wageComputation(Companyempwage companyEmployee){
+        System.out.println(companyEmployee.getCompanyName()+" Employee Wage Computation");
+
         // Variable
         int empHrs = 0;
         int totalEmpHrs = 0;
         int totalWorkDay = 0;
         int totalEmpWage = 0;
-
+        int empDailyWage = 0;
         while (totalWorkDay < companyEmployee.getNumOfWorkingDays() && totalEmpHrs <= companyEmployee.getMaxHrsInMonth()) {
 
             int employee = (int) ((Math.random() * 10) % 3);
@@ -37,16 +43,21 @@ public class EmployeeWage implements EmpWageBuilder{
             }
             totalWorkDay++;
             totalEmpHrs = totalEmpHrs + empHrs;
+            empDailyWage = empHrs * companyEmployee.getEmpRatePerHour();
+            empDailyMonthlyTotalWage.add( empDailyWage );
+            System.out.println("Day:"+totalWorkDay +" Wage is : "+empDailyWage );
         }
-        companyEmployee.setTotalEmpWage( totalEmpHrs * companyEmployee.empRatePerHr );
-        System.out.println("Employee Monthly Wage of " + companyEmployee.getCompanyName() + " is " + companyEmployee.getTotalEmpWage());
+        totalEmpWage=( totalEmpHrs * companyEmployee.getEmpRatePerHour());
+        empDailyMonthlyTotalWage.add( totalEmpWage );
+        companyEmployee.setTotalEmpWage( totalEmpWage );
+        System.out.println("Employee Monthly Total  Wage of " + companyEmployee.getCompanyName() + " is " + companyEmployee.getTotalEmpWage());
     }
     // Main Function
 
     public static void main(String arg[]) {
 
         System.out.println("WELCOME to EMPLOYEE WAGE Computation");
-
+        System.out.println();
         EmployeeWage emp = new EmployeeWage();
 
         // ArrayList of multiple companies wage
@@ -55,10 +66,10 @@ public class EmployeeWage implements EmpWageBuilder{
 
         company.add( new Companyempwage( "Thoughtworks", 40, 20, 100 ));
         emp.wageComputation(company.get(0));
-
+        System.out.println();
         company.add( new Companyempwage( "Wipro", 30, 22, 120 ));
         emp.wageComputation(company.get(1));
-
+        System.out.println();
         company.add( new Companyempwage( "Infosys", 45, 18, 90 ));
         emp.wageComputation(company.get(2));
     }
